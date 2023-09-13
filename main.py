72fe8961a164b844d427845796161d3f9bd88e6b
tinayiluo0322 <143360909+tinayiluo0322@users.noreply.github.com>
2023-09-13 19:14:18 -0400

    polars update

--- main.py
+++ main.py
@@ -3,9 +3,34 @@ Main code
 """
 
 
-def positive_real_number(a):
-    if a > 0:
-        return True
+import pandas as pd
+import matplotlib.pyplot as plt
+import polars as pl
 
 
-# if __name__ == "__main__":
+def summary(csv):
+    """summary statistics in csv"""
+    df = pl.read_csv(csv)
+    # print(df.describe())
+    # new_summary = df.describe()
+    # print(new_summary["age"][2])
+    # print(new_summary["age"][1])
+    # print(new_summary["age"][3])
+    # print(new_summary["age"][4])
+    return df.describe()
+
+
+def histogram_blood_pressure(csv):
+    """generate example visualization of the heart dataset"""
+    pd.set_option("display.max_columns", None)
+    polars_df = pl.read_csv(csv)
+    plt.figure(figsize=(10, 6))
+    plt.hist(polars_df["trtbps"], bins=20, edgecolor="black")
+    plt.title("Resting Blood Pressure Distribution")
+    plt.xlabel("Resting Blood Pressure")
+    plt.ylabel("Frequency")
+    plt.show()
+
+
+# summary("heart.csv")
+# histogram_blood_pressure("heart.csv")
--- test_main.py
+++ test_main.py
@@ -2,14 +2,27 @@
 Test goes here
 
 """
-from main import positive_real_number
+from main import summary, histogram_blood_pressure
 
 
-def test_positive_real_number():
-    # Test with a positive number
-    result = positive_real_number(5)
-    assert result, "Test failed for the positive number"
+def test_summary():
+    """Function calling summary which tests the mean and standard deviation value of the dataset"
+    the dataset"""
+    # mean of age column in heart dataset
+    # print(summary("heart.csv")["age"][0])
+    assert summary("heart.csv")["age"][0] == 54.366336633663366
+    # print(summary("heart.csv")["age"][1])
+    # standard deviation of age column in heart dataset
+    assert summary("heart.csv")["age"][1] == 9.082100989837857
+    # median of age column in heart dataset
+    assert summary("heart.csv")["age"][4] == 55.0
+
+
+def test_histogram_blood_pressure():
+    """Function calling histogram_blood_pressure()"""
+    histogram_blood_pressure("heart.csv")
 
 
 if __name__ == "__main__":
-    test_positive_real_number()
+    test_summary()
+    test_histogram_blood_pressure()
